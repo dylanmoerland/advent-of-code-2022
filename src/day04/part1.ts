a77c2e4699a30a3963671cf946a7cd478495d0ba
Dylan Moerland <dylan@headfwd.com>
2022-12-04 21:45:38 +0200

    day 04

--- src/day04/part1.ts
+++ src/day04/part1.ts
@@ -4,6 +4,13 @@ const data = readText('src/day04/data.txt')
 
 const result = data
   .split('\n')
+  .map((line) => line.split(',').map((group) => 
+    group.split('-').map((value) => parseInt(value, 10))
+  ))
+  .reduce((total, [[aStart, aEnd], [bStart, bEnd]]) => {
+    if ((aStart >= bStart && aEnd <= bEnd ) || (bStart >= aStart && bEnd <= aEnd))
+      return total + 1;
+    return total;
+  }, 0);
   
-
 console.log(result);
--- src/day04/part2.ts
+++ src/day04/part2.ts
@@ -4,5 +4,15 @@ const data = readText('src/day04/data.txt')
 
 const result = data
   .split('\n')
-
+  .map((line) => line.split(',').map((group) => 
+    group.split('-').map((value) => parseInt(value, 10))
+  ))
+  .map(([a,b]) => [
+    new Array(a[a.length - 1] - a[0] + 1).fill(0).map((_,index) => a[0] + index),
+    new Array(b[b.length - 1] - b[0] + 1).fill(0).map((_,index) => b[0] + index)
+  ])
+  .reduce((total, [a, b]) => {
+    return total + (a.some((value) => b.includes(value)) ? 1 : 0);
+  }, 0);
+  
 console.log(result);
