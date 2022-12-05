0330314da25444bd460c3c4ed379990551209fae
Dylan Moerland <dylan@headfwd.com>
2022-12-05 11:50:28 +0200

    Day 05

--- src/day04/part2.ts
+++ src/day04/part2.ts
@@ -4,12 +4,12 @@ const data = readText('src/day04/data.txt')
 
 const result = data
   .split('\n')
-  .map((line) => line.split(',').map((group) => 
-    group.split('-').map((value) => parseInt(value, 10))
-  ))
-  .map(([a,b]) => [
-    new Array(a[a.length - 1] - a[0] + 1).fill(0).map((_,index) => a[0] + index),
-    new Array(b[b.length - 1] - b[0] + 1).fill(0).map((_,index) => b[0] + index)
+  .map((line) => line.split(',').map((group) => {
+    return group.split('-').map((value) => parseInt(value, 10))
+  }))
+  .map(([a, b]) => [
+    new Array(a[a.length - 1] - a[0] + 1).fill(0).map((_, index) => a[0] + index),
+    new Array(b[b.length - 1] - b[0] + 1).fill(0).map((_, index) => b[0] + index)
   ])
   .reduce((total, [a, b]) => {
     return total + (a.some((value) => b.includes(value)) ? 1 : 0);
---
+++ src/day05/part1.ts
@@ -0,0 +1,43 @@
+import { readText } from '../helpers/readText';
+
+const data = readText('src/day05/data.txt')
+
+const [containerData, operationData] = data
+  .split('\n\n')
+
+const containers = containerData
+  .split('\n')
+  .reverse()
+  .slice(1)
+  .reduce<string[][]>((allContainers, line) => {
+    line.match(/.{1,4}/g).forEach((operationContainer, index) => {
+      const operation = operationContainer.charAt(1);
+
+      if (operation === ' ') return;
+
+      if (Array.isArray(allContainers[index])) {
+        allContainers[index].push(operation)
+      } else {
+        allContainers[index] = [operation]
+      }
+    })
+    return allContainers;
+  }, []);
+
+const operations = operationData
+  .split('\n')
+  .map((line) => line.match(/[0-9]{1,10}/g).map((number) =>{
+    return parseInt(number, 10);
+  }));
+
+operations
+  .forEach(([amount, from, to]) => {
+    const slice = containers[from - 1].splice(containers[from - 1].length - amount);
+    containers[to - 1].push(...slice.reverse());
+  });
+
+const result = containers
+  .map((container) => container[container.length - 1])
+  .join('');
+  
+console.log(result);
